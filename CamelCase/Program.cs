276f42da3216cbfab40f423fab76368b0f7bbf63
ahmet bilgic <ahmet_bilgic@yahoo.com>
2022-03-14 22:02:47 +0300

    SM case is done.

--- CamelCase/Program.cs
+++ CamelCase/Program.cs
@@ -1,9 +1,17 @@
 ﻿
-//S;M;plasticCup() => plastic cup
+/// <summary>
+/// S;M;plasticCup() => plastic cup
+/// C;V;mobile phone => mobilePhone
+/// C;C;coffee machine => CoffeeMachine
+/// S;C;LargeSoftwareBook => large software book
+/// C;M;white sheet of paper => whiteSheetOfPaper()
+/// S;V;pictureFrame => picture frame
+/// </summary>
 
-Console.WriteLine("Hello, World!");
 
-var cline = Console.ReadLine();
+
+//var cline = Console.ReadLine();
+var cline = "S;M;plasticCup()";
 
 var splitted = cline.Split(';');
 
@@ -12,7 +20,18 @@ if (splitted[0] == "S")
     if (splitted[1] == "M")
     {
         int uindex = UpperIndex(splitted[2]);
+        if (uindex != 999)
+        {
+            char[] charstrim = { '(', ')' };
 
+            var str1 = splitted[2].Substring(0, uindex);
+            var lower = char.ToLower(splitted[2][uindex]);
+
+            var str2 = splitted[2].Substring(uindex + 1, splitted[2].Length - uindex - 1);
+            str2 = str2.Trim(charstrim);
+            var resultstr = string.Concat(str1, " ", lower.ToString(), str2);
+            Console.WriteLine(resultstr);
+        }
 
     }
 
@@ -21,12 +40,14 @@ if (splitted[0] == "S")
 
 int UpperIndex(string str)
 {
-    foreach (char c in str)
-    {
-        if (char.IsUpper(c))
-        {
+    int retval = 999;
 
+    for (int i = 0; i < str.Length; i++)
+    {
+        if (char.IsUpper(str[i]))
+        {
+            retval = i;
         }
     }
-    return 0;
+    return retval;
 }
