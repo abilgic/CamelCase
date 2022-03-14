96ede215179a6a39d085bcc5469edec83dce0822
ahmet bilgic <ahmet_bilgic@yahoo.com>
2022-03-14 21:01:38 +0300

    Add project files.

---
+++ CamelCase/Program.cs
@@ -0,0 +1,32 @@
+﻿
+//S;M;plasticCup() => plastic cup
+
+Console.WriteLine("Hello, World!");
+
+var cline = Console.ReadLine();
+
+var splitted = cline.Split(';');
+
+if (splitted[0] == "S")
+{
+    if (splitted[1] == "M")
+    {
+        int uindex = UpperIndex(splitted[2]);
+
+
+    }
+
+}
+
+
+int UpperIndex(string str)
+{
+    foreach (char c in str)
+    {
+        if (char.IsUpper(c))
+        {
+
+        }
+    }
+    return 0;
+}
